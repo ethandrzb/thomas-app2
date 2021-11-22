9789b31e82dbfa144021ed5d3496a8a7115d5820
ethandrzb <ethandrzb@gmail.com>
2021-11-21 19:53:52 -0500

    Finished unit tests for InventoryValidator

--- GraphicalInventoryManager/src/test/java/logic/InventoryValidatorTest.java
+++ GraphicalInventoryManager/src/test/java/logic/InventoryValidatorTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryValidatorTest
 {
+    Inventory inventory;
     InventoryValidator validator;
 
     @BeforeEach
@@ -19,6 +20,13 @@ class InventoryValidatorTest
     {
         validator = new InventoryValidator();
         validator.setErrorDialogsEnabled(false);
+
+        inventory = new Inventory();
+
+        // Add dummy data
+        inventory.addItem("item 1", "A-XXX-XXX-XXX", 1200.123);
+        inventory.addItem("item 2", "A-XXX-XXX-XXW", 654.45);
+        inventory.addItem("item 3", "A-XXX-XXX-XXV", 9.09);
     }
 
     @Test
@@ -63,25 +71,83 @@ class InventoryValidatorTest
     @Test
     void isValidSerial_Positive()
     {
+        assertTrue(validator.isValidSerial(inventory, "A-XXX-XXX-XXU", ""));
+    }
+
+    // Incorrectly formatted serial
+    @Test
+    void isValidSerial_Negative_Case_1()
+    {
+        assertFalse(validator.isValidSerial(inventory, "A-XXX-XXX-XX", ""));
+    }
+
+    // Duplicate serial
+    @Test
+    void isValidSerial_Negative_Case_2()
+    {
+
+        assertFalse(validator.isValidSerial(inventory, "A-XXX-XXX-XXX", ""));
     }
 
     @Test
-    void isValidSerial_Negative() {
+    void isValidMonetaryValue_Positive()
+    {
+        assertTrue(validator.isValidMonetaryValue("1.23"));
+
+        assertTrue(validator.isValidMonetaryValue("1.20"));
+
+        assertTrue(validator.isValidMonetaryValue("1.2"));
+
+        assertTrue(validator.isValidMonetaryValue("1.0"));
+
+        assertTrue(validator.isValidMonetaryValue("1.00"));
+
+        assertTrue(validator.isValidMonetaryValue("0.09"));
+    }
+
+    // Incorrectly formatted number
+    @Test
+    void isValidMonetaryValue_Negative_Case_1()
+    {
+        assertFalse(validator.isValidMonetaryValue("."));
+
+        assertFalse(validator.isValidMonetaryValue("q.0"));
+
+        assertFalse(validator.isValidMonetaryValue("0.jh"));
+
+        assertFalse(validator.isValidMonetaryValue("asd.dc"));
+    }
+
+    // Value must greater than zero
+    @Test
+    void isValidMonetaryValue_Negative_Case_2()
+    {
+        assertFalse(validator.isValidMonetaryValue("0.00"));
+
+        assertFalse(validator.isValidMonetaryValue("0.0"));
+
+        assertFalse(validator.isValidMonetaryValue("0."));
+
+        assertFalse(validator.isValidMonetaryValue("0"));
     }
 
     @Test
-    void isValidMonetaryValue_Positive() {
+    void validateAllInputs_Positive()
+    {
+        assertTrue(validator.validateAllInputs(inventory, "test name", "A-YYY-ZZZ-WWW", "321.12"));
     }
 
     @Test
-    void isValidMonetaryValue_Negative() {
-    }
+    void validateAllInputs_Negative()
+    {
+        // Repeat of selected individual tests
 
-    @Test
-    void validateAllInputs_Positive() {
-    }
+        assertFalse(validator.validateAllInputs(inventory, "t", "A-YYY-ZZZ-WWW", "321.12"));
 
-    @Test
-    void validateAllInputs_Negative() {
+        assertFalse(validator.validateAllInputs(inventory, "test name", "A-YYY-ZZZ-WW", "321.12"));
+
+        assertFalse(validator.validateAllInputs(inventory, "test name", "A-XXX-XXX-XXV", "321.12"));
+
+        assertFalse(validator.validateAllInputs(inventory, "test name", "A-XXX-XXX-XXG", "0"));
     }
 }
